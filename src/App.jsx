42b8cc935b00f405a6806a59f48f1c31e4c212ba
Felipe Frizzo <felipefrizzovg@gmail.com>
2024-07-03 15:01:29 -0300

    Finished Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,31 @@
 import { Post } from "./Post";
 import { Header } from "./components/Header";
 
+import styles from './App.module.css'
+
 import './global.css'
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <>
       <Header />
-      <Post
-        author="Felipe Frizzo"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi placeat eum delectus amet ducimus aliquam autem, natus vel, aliquid dicta dolores libero reiciendis incidunt deserunt laboriosam neque eveniet hic eligendi?"
-      />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Felipe Frizzo"
+            content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi placeat eum delectus amet ducimus aliquam autem, natus vel, aliquid dicta dolores libero reiciendis incidunt deserunt laboriosam neque eveniet hic eligendi?"
+          />
 
-      <Post
-        author="Raphael Frizzo"
-        content="Segundo post"
-      />
+          <Post
+            author="Raphael Frizzo"
+            content="Segundo post"
+          />
+        </main>
+      </div>
+
     </>
   );
 }
