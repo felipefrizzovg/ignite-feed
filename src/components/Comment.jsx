19a602c59101fadfc0748405e558c1e080c110d9
Felipe Frizzo <felipefrizzovg@gmail.com>
2024-07-04 21:51:56 -0300

    Added comment section

---
+++ src/components/Comment.jsx
@@ -0,0 +1,37 @@
+import { ThumbsUp, Trash } from "@phosphor-icons/react";
+import styles from "./Comment.module.css";
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/felipefrizzovg.png" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Felipe Frizzo</strong>
+
+              <time title="03 de Julho às 15:18" dateTime="2024-07-03 15:18:00">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar Comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
